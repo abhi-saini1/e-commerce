92a1e207f573fa5581e9e33d5ee404894a8e7610
abhi-saini1 <abhishek977933@gmail.com>
2024-05-03 15:33:31 +0530

    commit

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,8 +1,10 @@
 'use client';
+import FormatPrice from '@/types/FormatPrice';
 import { ProductType } from '@/types/ProductTypes';
 import Image from 'next/image';
 import React, { useState } from 'react'
 import toast from 'react-hot-toast';
+import { FiHeart } from "react-icons/fi";
 
 const ProductCard = ({product,}:{product:ProductType}) => {
   const [selectedSize,setSelectedSize] =useState('');
@@ -11,9 +13,32 @@ const ProductCard = ({product,}:{product:ProductType}) => {
     toast.error('Please choose a size first')
   }
   return (
-    <div className='relative flex flex-col'>
-      <div className=''>
-        <Image src={product.image} alt='' height={300} width={300}/>
+    <div className='relative flex flex-col items-center '>
+      <div className='relative group border duration-300 hover:scale-100 '>
+        <Image src={product.image} alt={`image of ${product.name}`} className='cursor-pointer w-[250px] h-[300px]' height={300} width={300}/>
+      
+        <div className='absolute top-5 right-0 items-center justify-center gap-3'>
+          <button className='mx-2  text-gray-900 p-2 rounded-md hover:text-red-500'>
+          <FiHeart/>
+          </button>
+          
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+          <h2 className='font-bold'>{product.name}</h2>
+          <span>{FormatPrice(product.unit_amount)}</span>
+        </div>
+        <div className='flex  items-center justify-center gap-2'>
+        <select value={selectedSize} onChange={(e)=> setSelectedSize(e.target.value)}
+        className='my-2 p-2 border rounded-md'>
+          <option value=''>Select Size</option>
+          <option value='UK-6'>UK-6</option>
+          <option value='UK-7'>Uk-7</option>
+          <option value='UK-8'>UK-8</option>
+          <option value='Uk-9'>UK-9</option>
+          <option value='UK-10'>UK-10</option>
+        </select>
+        <p>Add to Cart</p>
+        </div>
       </div>
     </div>
   )
---
+++ src/types/FormatPrice.ts
@@ -0,0 +1,8 @@
+const FormatPrice = (amount: number) => {
+    return new Intl.NumberFormat('en-US',{
+        style: 'currency',
+        currency: 'INR',
+    }).format(amount / 100)
+};
+
+export default FormatPrice
